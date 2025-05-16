dd07fa30b9eeace8ceab005eac6b22e6d17459a8
ABDEL MONIM MAZGOURA <135391027+ABDE777@users.noreply.github.com>
2025-05-16 18:49:17 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -130,14 +130,13 @@ const HeroSection: React.FC<HeroSectionProps> = ({ onJoinClick }) => {
                 </div>
 
                 <motion.div whileHover={{ scale: 1.03 }}>
-  <a href="#announcements" className="group">
+  <a href="#announcements" className="group" disabled>
     <CustomButton
       size="sm"
       className="bg-primary/85 hover:bg-primary text-white"
     >
       <span className="flex items-center gap-1.5 text-sm">
-        Voir détails
-        <ArrowRight className="h-4 w-4 transition-transform group-hover:translate-x-0.5" />
+        Soyer Prets
       </span>
     </CustomButton>
   </a>
